6905ca5f5a4110c0138a6efd0b0bbff8cca77177
Victoria-Developer <77235839+Victoria-Developer@users.noreply.github.com>
2025-08-18 20:03:27 +0800

    Added pst splash theme and preferred icon for api 33 and above.

--- android/app/src/main/kotlin/com/fitsy/fitsy/MainActivity.kt
+++ android/app/src/main/kotlin/com/fitsy/fitsy/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.fitsy.fitsy
 
-import android.os.Build
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(b: Bundle?) {
-        // Aligns the Flutter view vertically with the window.
-        WindowCompat.setDecorFitsSystemWindows(window, false)
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            // Disable the Android splash screen fade out animation to avoid
-            // a flicker before the similar frame is drawn in Flutter.
-            splashScreen.setOnExitAnimationListener { splashScreenView -> splashScreenView.remove() }
-        }
         installSplashScreen()
         super.onCreate(b)
     }
